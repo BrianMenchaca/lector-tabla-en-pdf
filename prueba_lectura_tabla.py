be8a5042422b5d8b23b54f6fb97d63a82d4cc6ba
Brian Menchaca <brianmenchaca@gmail.com>
2022-12-26 11:29:59 -0300

    Se modifico prueba_lectura_tabla para que mande los datos a la base de datos

--- prueba_lectura_tabla.py
+++ prueba_lectura_tabla.py
@@ -5,6 +5,7 @@ import os
 import openpyxl
 import convertir_pdf_a_jpg as cpdf
 import shutil
+import teradatasql
 
 
 def main():
@@ -27,7 +28,7 @@ def main():
 
     if (os.path.exists(dir_procesados) == False):
         os.mkdir(dir_procesados)
-    
+
     if (os.path.exists(dir_test_folder) == False):
         os.mkdir(dir_test_folder)
 
@@ -45,12 +46,12 @@ def main():
     for pdf in contenido_pdf:
         if os.path.isfile(os.path.join(dir, pdf)) and pdf.endswith('.pdf'):
             clear()
-            print("*"*50)
+            print("*" * 50)
             print("\nSe leyo el archivo: " + pdf + "\n")
-            print("*"*50)
+            print("*" * 50)
             cpdf.convertir_pdf(pdf)
-            nombre_txt = pdf[:-4] + ".txt" # Nombre de la salida en formato .txt
-            nombre_excel = pdf[:-4] + ".xlsx" # Nombre de la salida en formato .xlsx (Excel)
+            nombre_txt = pdf[:-4] + ".txt"  # Nombre de la salida en formato .txt
+            nombre_excel = pdf[:-4] + ".xlsx"  # Nombre de la salida en formato .xlsx (Excel)
 
             dir_img_pdf = dir_img + "/" + pdf[:-4]
 
@@ -62,13 +63,18 @@ def main():
             table = []
 
             for imagen in contenido_img:
-                table_temp = leer_tabla(dir_img_pdf + "/" + imagen, language, dir_test_folder + "/" + imagen[:-4])
+                table_temp = leer_tabla(dir_img_pdf + "/" + imagen, language,
+                                        dir_test_folder + "/" + imagen[:-4])
                 table = table + table_temp
+
+            data = leer_datos_factura(dir_test_folder + "/" + imagen[:-4])
+
+            send_data(table, data)
 
             save_in_excel(table, dir_excel + "/" + nombre_excel)
             write_txt(table, dir_txt + "/" + nombre_txt)
 
-            shutil.move(dir + '/' + pdf, dir_procesados + "/" + pdf)
+            # shutil.move(dir + '/' + pdf, dir_procesados + "/" + pdf)
             input("\nPresione Enter para continuar...")
 
 
@@ -131,7 +137,7 @@ def leer_tabla(src, language, test_folder):
     # básicamente la altura (salto de coordenadas y) y la longitud (salto de coordenadas x) de la tabla de celdas.
     i = 0
     sort_y_point = np.sort(ys)
-    
+
     if len(sort_y_point) == 0:
         return []
 
@@ -177,12 +183,18 @@ def leer_tabla(src, language, test_folder):
         # cell = raw[y_point_arr[j] - dif:y_point_arr[j + 1] - dif, xs[i]:xs[i + 1]]
         cell = gray[y_point_arr[j] - dif:y_point_arr[j + 1] - dif,
                     xs[i]:xs[i + 1]]
+
         cv2.imwrite(
-            test_folder + "/tabla_final_" + str(j) + "-" + str(i) + ".png", cell)
+            test_folder + "/tabla_final_" + str(j) + "-" + str(i) + ".png",
+            cell)
 
         # Leer texto
         cv2.imwrite(test_folder + "/temp_img.png", cell)
         image = cv2.imread(test_folder + "/temp_img.png")
+
+        # Elimina de la imagen, la celda leida
+        raw[y_point_arr[j] - dif:y_point_arr[j + 1] - dif,
+            xs[i]:xs[i + 1]] = (0, 0, 0)
 
         # Parametros
         low_text_value = 0.08  # Tamaño mínimo de caracter
@@ -221,6 +233,25 @@ def leer_tabla(src, language, test_folder):
         else:
             data[j].append(text1)
 
+    # Guarda la imagen sin las celdas leidas
+    cv2.imwrite(test_folder + "/temp_img_data" + ".png", raw)
+
+    data = data_cleansing(data)
+
+    return data
+
+
+def data_cleansing(data):
+    # Elimina el encabezado de la tabla
+    data.pop(0)
+
+    # Eliminar duplicados
+    set_data = set(data)
+    data = list(set_data)
+
+    # Eliminar filas que no tengan 6 elementos o no tengan la clave primaria
+    data = list(filter(lambda item: item[0] != '' and len(item) == 6, data))
+
     return data
 
 
@@ -256,13 +287,115 @@ def save_in_excel(data, name):
     wb.save(name)
 
 
+# Leo la tabla sin las celdas ya leidas
+def leer_datos_factura(test_folder):
+
+    reader = easyocr.Reader(["es"], gpu=False)
+    image = cv2.imread(test_folder + "/temp_img_data" + ".png")
+
+    text_list = reader.readtext(image, detail=0)
+
+    data_list = ["" for i in range(0, 6)]
+
+    clave = "N.' de factura:"
+    index = text_list.index(clave)
+    data_list[0] = text_list[index + 1]
+
+    clave = "Factura para:"
+    index = text_list.index(clave)
+    data_list[1] = text_list[index + 1]
+
+    clave = "Teléfono: "
+    temp_list = list(filter(lambda x: x.startswith(clave), text_list))
+    data_list[2] = temp_list[0]
+    data_list[2] = data_list[2][len(clave):]
+
+    clave = "Dirección:"
+    index = text_list.index(clave)
+    data_list[3] = text_list[index + 1]
+
+    clave = "Fax:"
+    index = text_list.index(clave)
+    data_list[4] = text_list[index + 1]
+
+    clave = "Fecha de la factura:"
+    index = text_list.index(clave)
+    data_list[5] = text_list[index + 1]
+
+    return data_list
+
+
+# Envia los datos a la base de datos
+def send_data(table, data):
+
+    try:
+        # Crea la conexion con la base de datos
+        cnx = teradatasql.connect(host='192.168.0.14',
+                                  database='brianme',
+                                  user='dbc',
+                                  password='dbc')
+        cursor = cnx.cursor()
+
+        data[5] = corregir_formato_fecha(data[5])
+
+        # Verifica los datos a ingresar a la tabla
+        print("*" * 40)
+        for item in data:
+            print(item)
+        print("*" * 40)
+
+        cursor.execute("""INSERT INTO brianme.Factura (
+            NumFactura,
+            FacturaPara,
+            Telefono,
+            Direccion,
+            Fax,
+            FechaEmision)
+            VALUES ( """ + "'" + data[0] + "'," + "'" + data[1] + "'," + "'" +
+                       data[2] + "'," + "'" + data[3] + "'," + "'" + data[4] +
+                       "'," + "'" + data[5] + "'"
+                       ");")
+
+            
+
+        # Consulta usando python
+        # cursor.execute(""" select * from Factura; """)
+        # result = cursor.fetchall()
+        # print(result)
+
+    except teradatasql.OperationalError:
+        print("Datos duplicados")
+    finally:
+        # Cierra la conexion
+        cnx.close()
+
+
 # Escribo en txt e imprimo en pantalla los datos
 def write_txt(data, name):
     print()
     with open(name, "w") as t:
         for index, item in enumerate(data):
-            print(index, ":", item)
+            # print(index, ":", item)
             t.write(str(item) + "\n")
+
+
+# Corregir el formato de fecha
+def corregir_formato_fecha(date):
+    day, month, year = date.split('/')
+
+    day = day.strip()
+    if len(day) < 2:
+        day = "0" + day
+
+    month = month.strip()
+    if len(month) < 2:
+        month = "0" + month
+
+    year = year.strip()
+    if len(year) < 3:
+        year = "20" + year
+
+    return day + "/" + month + "/" + year
 
 
 # Limpiar consola
@@ -271,6 +404,7 @@ def clear():
         os.system("cls")
     else:
         os.system("clear")
+
 
 if __name__ == "__main__":
     main()
